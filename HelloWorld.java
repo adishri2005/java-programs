0d0f75aa56c5bd0adc8b5798811cffeaf19315df
Aditya Shrivastava <adityashrivastava2005@gmail.com>
2025-01-30 00:29:14 +0530

    EXPERIMENT 1 (SAMPLE)

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+  {
+        System.out.println("Hello, World!");
+        System.out.println("Aditya Shrivastava");
+        System.out.println("Bhopal / Dehradun");
+        System.out.println("Madhya Pradesh / Uttarakhand");
+  }
+}
